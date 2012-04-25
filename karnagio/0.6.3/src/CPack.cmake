9c5a19879e7dab5cd66493cbbf1ffcc163a98d9f
Nikolaos Vasiloglou <nvasil@ismion.com>
2012-04-24 23:14:10 -0400

    changed the package

--- karnagio/0.6.3/src/CPack.cmake
+++ karnagio/0.6.3/src/CPack.cmake
@@ -1,41 +1,63 @@
 INCLUDE(InstallRequiredSystemLibraries)
-file(GLOB libmlpack_dev /.${CMAKE_BINARY_DIR}/${GenCMake_LIB_OUT_DIR}/libmlpack-dev*)
-install(FILES  ${libmlpack_dev}
-  DESTINATION lib)
-file(GLOB libfastlib /.${CMAKE_BINARY_DIR}/${GenCMake_LIB_OUT_DIR}/libfastlib*)
-install(FILES ${libfastlib}
-  DESTINATION lib)
-file(GLOB libubigraph /.${CMAKE_BINARY_DIR}/${GenCMake_LIB_OUT_DIR}/libubigraph*)
-install(FILES ${libubigraph}
-   DESTINATION lib)
-install(FILES ../include/fastlib/base/base.h
-  DESTINATION include)
-install(FILES ../include/fastlib/base/basic_types.h
-  DESTINATION include)
-install(FILES ../include/fastlib/base/constant_strings.h
-  DESTINATION include)
-install(FILES ../include/fastlib/base/logger.h
-  DESTINATION include)
-install(FILES ../include/fastlib/base/null_stream.h
-  DESTINATION include)
-file(GLOB external_files /.${CMAKE_BINARY_DIR}/../include/fastlib/external/*.h)
-foreach(FILE ${external_files})
-  install(FILES /.${FILE}
-    DESTINATION include)
-endforeach()
-file(GLOB external_files /.${CMAKE_BINARY_DIR}/../include/mlpack/external/*.h)
-foreach(FILE ${external_files})
-  install(FILES /.${FILE}
-    DESTINATION include)
-endforeach()
-
-
-SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Paper Boat")
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "PaperBoat Karnagio")
 SET(CPACK_PACKAGE_VENDOR "Ismion Inc.")
+SET(CPACK_PACKAGE_CONTACT " Ismion <nvasil@ismion.com>")
+SET(CPACK_SOURCE_GENERATOR TGZ)
 SET(CPACK_PACKAGE_VERSION_MAJOR "0")
 SET(CPACK_PACKAGE_VERSION_MINOR "6")
-SET(CPACK_PACKAGE_VERSION_PATCH "0")
-SET(CPACK_PACKAGE_NAME MLPACK-DEV)
-SET(CPACK_ZIP_COMPONENT_INSTALL ON)
+SET(CPACK_PACKAGE_VERSION_PATCH "3")
+SET(CPACK_PACKAGE_NAME paperboat-dev)
+SET(CPACK_GENERATOR "DEB")
+SET(CPACK_DEBIAN_PACKAGE_NAME "paperboat")
 
-INCLUDE(CPack)
+if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+  set ( ARCH64BIT 1 )
+else ()
+  set ( ARCH64BIT 0 )
+endif ()
+message ("-- 64bit architecture is ${ARCH64BIT}")
+if ( ${ARCH64BIT} EQUAL 1 )
+  SET ( CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
+else( ${ARCH64BIT} EQUAL 1 )
+  SET ( CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i686")
+endif ( ${ARCH64BIT} EQUAL 1 )
+
+SET(CPACK_DEBIAN_PACKAGE_DEPENDS 
+  "libboost-all-dev, liblapack-dev, g++, gfortran, libtrilinos-dev")
+
+SET(CPACK_SOURCE_IGNORE_FILES
+        "*~"
+        "*.swp"
+        "*.swo"
+        "*.log"
+        "a.*"
+        "temp"
+        )
+
+install(FILES  /.${CMAKE_BINARY_DIR}/${GenCMake_BIN_OUT_DIR}/allkn
+  DESTINATION /opt/ismion/paperboat/)
+install(FILES  /.${CMAKE_BINARY_DIR}/${GenCMake_BIN_OUT_DIR}/kde
+  DESTINATION /opt/ismion/paperboat/)
+install(FILES  /.${CMAKE_BINARY_DIR}/${GenCMake_BIN_OUT_DIR}/kmeans
+  DESTINATION /opt/ismion/paperboat/)
+install(FILES  /.${CMAKE_BINARY_DIR}/${GenCMake_BIN_OUT_DIR}/lasso
+  DESTINATION /opt/ismion/paperboat/)
+install(FILES  /.${CMAKE_BINARY_DIR}/${GenCMake_BIN_OUT_DIR}/regression
+  DESTINATION /opt/ismion/paperboat/)
+install(FILES  /.${CMAKE_BINARY_DIR}/${GenCMake_BIN_OUT_DIR}/svd
+  DESTINATION /opt/ismion/paperboat/)
+install(FILES  /.${CMAKE_BINARY_DIR}/${GenCMake_BIN_OUT_DIR}/svm
+  DESTINATION /opt/ismion/paperboat/)
+install(FILES  /.${CMAKE_BINARY_DIR}/${GenCMake_BIN_OUT_DIR}/nmf
+  DESTINATION /opt/ismion/paperboat/)
+install(FILES  /.${CMAKE_BINARY_DIR}/${GenCMake_BIN_OUT_DIR}/npr
+  DESTINATION /opt/ismion/paperboat/)
+
+
+include (CPack)
+
+
+
+
+
+
