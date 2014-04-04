414600f64c0cb70fd2bf5e3aa5b81ec7d80b7808
vasiloglou <nvasil@ismion.com>
2014-04-04 15:28:01 -0400

    bug fixes

--- karnagio/0.6.3/src/lib/mlpack-dev/Boost.gen.cmake
+++
@@ -1,8 +0,0 @@
-set(Boost_USE_STATIC_LIBS   ON)
-find_package(Boost COMPONENTS program_options serialization system)
-
-list(APPEND GenCMake_LIBRARIES ${Boost_LIBRARIES})
-
-include_directories(${Boost_INCLUDE_DIRS})
-
-#add_definitions(-DBOOST_ALL_DYN_LINK)
